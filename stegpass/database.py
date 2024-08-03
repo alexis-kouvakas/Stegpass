90f4f5d27ece6829b6805808eae3f2dc1aa15013
Jaden <43506164+soravoid@users.noreply.github.com>
2024-08-02 22:15:47 -0500

    Change 'Passwords' to 'Logins' table and add 'LoginName' column

--- stegpass/database.py
+++ stegpass/database.py
@@ -5,9 +5,10 @@ from pathlib import Path
 from sqlite3 import DatabaseError
 from typing import TYPE_CHECKING
 
+import pysqlcipher3.dbapi2 as sqlite
+
 from stegpass.database_exceptions import PasswordValidationError
 
-from pysqlcipher3 import dbapi2 as sqlite
 
 if TYPE_CHECKING:
     from sqlite3 import Cursor
@@ -23,12 +24,15 @@ def create_vault(vault_path: Path, master_password: str) -> bool:
     if vault_path.exists():
         return False
     with sqlite.connect(vault_path) as conn:
-        cursor = conn.cursor() 
+        cursor = conn.cursor()
         # TODO Sanitize master_password
         cursor.execute(f"PRAGMA key='{master_password}';")
         cursor.execute(
-            "CREATE TABLE Passwords "
-            "(PasswordId INTEGER PRIMARY KEY, Password TEXT NOT NULL);"
+            "CREATE TABLE Logins("
+            "LoginId INTEGER PRIMARY KEY, "
+            "LoginName TEXT NOT NULL, "
+            "Password TEXT NOT NULL"
+            ");"
         )
         cursor.close()
         conn.commit()
@@ -41,26 +45,26 @@ def access_vault(
     master_password: str,
     should_commit: bool = False,
 ) -> Iterator["Cursor | Exception"]:
-    """Attempt to connect to the database at `vault_path` with `master_password`.
+    """Attempt to connect to a database with a master password.
 
     :param vault_path: Path to the new vault
     :param master_password: The password to lock the vault with
     :yield: Connection object if successful, otherwise a subclass of Exception
     """
     if not vault_path.exists():
-        return FileNotFoundError(f"\"{vault_path}\" does not exist")
+        yield FileNotFoundError(f"\"{vault_path}\" does not exist")
     else:
-        conn =  sqlite.connect(vault_path)
+        conn = sqlite.connect(vault_path)
         cursor = conn.cursor()
         cursor.execute(f"PRAGMA key='{master_password}';")
         try:
             # Checking if the password was correct
             cursor.execute("PRAGMA schema_version;")
-        except DatabaseError as e:
-            return PasswordValidationError(e)
+        except DatabaseError:
+            yield PasswordValidationError()
         # Must yield conn within try-finally so it is properly closed
         try:
-            yield cursor 
+            yield cursor
         finally:
             if should_commit:
                 conn.commit()
